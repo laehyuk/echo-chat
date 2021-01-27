506c9abd7db38238e8e97466ba75d5a4cc371974
leo <lhjang@mybroccoli.co.kr>
2021-01-27 22:25:09 +0900

    add: test config

--- server_test.go
+++ server_test.go
@@ -1,8 +1,37 @@
 package main
 
 import (
+	"github.com/gorilla/websocket"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
-	"github.com/smartystreets/goconvey/convey"
 )
 
+func shouldGetEchoText(t *testing.T)  {
+	conn := httptest.NewServer(http.HandlerFunc(echo))
+	defer conn.Close()
 
+	u := "ws" + strings.TrimPrefix(conn.URL, "http")
+
+	ws, _, err := websocket.DefaultDialer.Dial(u, nil)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	defer ws.Close()
+
+	// Send message to server, read response and check to see if it's what we expect.
+	for i := 0; i < 10; i++ {
+		if err := ws.WriteMessage(websocket.TextMessage, []byte("hello")); err != nil {
+			t.Fatalf("%v", err)
+		}
+		_, p, err := ws.ReadMessage()
+		if err != nil {
+			t.Fatalf("%v", err)
+		}
+		if string(p) != "hello" {
+			t.Fatalf("bad message")
+		}
+	}
+
+}
